a27d34a6fa037dd7c063e4acbc37bf62e9a7c18a
Madhawa Monarawila <madawammonarawila@gmail.com>
2023-07-16 16:04:02 +0530

    Add react-mic-plus and send recording to backend

--- src/App.js
+++ src/App.js
@@ -1,10 +1,74 @@
 import React, { useState } from "react";
 import { Container, Button, Alert } from "react-bootstrap";
 
+import { ReactMicPlus } from "react-mic-plus";
+import axios from "axios";
+
+const nodeAppUrl = "http://localhost:3000/postAudio";
+
+const Recorder = () => {
+  const [recording, setRecording] = useState(false);
+  const [blob, setBlob] = useState(null);
+  const [transcript, setTranscript] = useState("");
+
+  const handleStart = () => {
+    setRecording(true);
+    setBlob(null);
+    setTranscript("");
+  };
+
+  const handleStop = (recordedBlob) => {
+    setRecording(false);
+    setBlob(recordedBlob);
+  };
+
+  const handleSubmit = async () => {
+    if (blob) {                         // checks the blob is not null
+      const formData = new FormData();
+      formData.append("audio", blob.blob);
+
+      try {
+        const { data } = await axios.post(nodeAppUrl, formData);
+
+        if (data.transcript) {
+          setTranscript(data.transcript);
+        } else {
+          setTranscript("No transcript found.");
+        }
+      } catch (err) {
+        console.error(err);
+        setTranscript("An error occurred.");
+      }
+    }
+  };
+
+  return (
+    <div>
+      <ReactMicPlus
+        record={recording}
+        className="sound-wave"
+        onStop={handleStop}
+        strokeColor="#000000"
+        backgroundColor="#FF4081"
+      />
+      <Button variant="primary" onClick={handleStart} disabled={recording}>
+        Start
+      </Button>
+      <Button variant="secondary" onClick={handleStop} disabled={!recording}>
+        Stop
+      </Button>
+      <Button variant="success" onClick={handleSubmit} disabled={!blob}>
+        Submit
+      </Button>
+    </div>
+  );
+};
+
 const App = () => {
   return (
     <Container className="mt-4">
       <h1>Speech to Text App</h1>
+      <Recorder />
     </Container>
   );
 };
